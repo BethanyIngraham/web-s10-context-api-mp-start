a6f78dbb481f5d1927d087fce39bf19dfa4270a7
Bethany Ingraham <BethFIngraham@gmail.com>
2024-04-26 21:00:49 -0400

    solution for module proj. with notes

--- frontend/components/QuoteForm.js
+++ frontend/components/QuoteForm.js
@@ -1,4 +1,7 @@
-import React, { useReducer } from 'react'
+import React, { useReducer, useContext } from 'react'
+import { QuotesContext } from '../context/quotesContext'
+
+// the form manages its own state, because the rest of the application doesn't need its values
 
 const CHANGE_INPUT = 'CHANGE_INPUT'
 const RESET_FORM = 'RESET_FORM'
@@ -21,15 +24,20 @@ const reducer = (state, action) => {
   }
 }
 
-export default function TodoForm(props) {
-  const { createQuote } = props
+export default function TodoForm() {
+ // test that the data is passing through -> const data = useContext(QuotesContext) -> console.log(data)
+  const { createQuote } = useContext(QuotesContext) // switch out props for useContext(QuotesContext)
+
   const [state, dispatch] = useReducer(reducer, initialState)
+
   const onChange = ({ target: { name, value } }) => {
     dispatch({ type: CHANGE_INPUT, payload: { name, value } })
   }
+
   const resetForm = () => {
     dispatch({ type: RESET_FORM })
   }
+
   const onNewQuote = evt => {
     evt.preventDefault()
     const { authorName, quoteText } = state
--- frontend/components/Quotes.js
+++ frontend/components/Quotes.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { QuotesContext } from '../context/quotesContext'
 
-export default function Quotes(props) {
+export default function Quotes() {
+  // test that the data is passing through -> const data = useContext(QuotesContext) -> console.log(data)
   const {
     quotes,
     highlightedQuote,
@@ -9,7 +11,7 @@ export default function Quotes(props) {
     editQuoteAuthenticity,
     setHighlightedQuote,
     toggleVisibility,
-  } = props
+  } = useContext(QuotesContext) // switch out props for useContext(QuotesContext)
   return (
     <div id="quotes">
       <h3>Quotes</h3>
